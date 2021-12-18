04e2cf9c36aabec75a9b38efa2761f6f2155a384
Maksim Chekhovskiy <veles.cafe@gmail.com>
2021-12-18 13:08:41 +0200

    в

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-$(function(){
+$(function () {
+    $('.popup__link').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
 
+		fixedContentPos: false
+	});
 });
